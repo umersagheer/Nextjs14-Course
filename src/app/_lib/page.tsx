357fde62596b0abe671141d5c2c47a7cba29c2fc
Muhammad Umer <mhadisb1212@gmail.com>
2023-12-30 23:53:07 +0500

    Private Folder

---
+++ src/app/_lib/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PrivateRoute = () => {
+  return <div>Private route</div>;
+};
+
+export default PrivateRoute;
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,5 @@
+import Button from "@/components/Button";
+import Link from "next/link";
 import React from "react";
 
 const NotFound = () => {
@@ -5,6 +7,10 @@ const NotFound = () => {
     <div className="h-screen bg-gray-900 flex flex-col justify-center items-center text-3xl font-bold text-white">
       Not Found
       <p className="text-5xl">There is nothing here</p>
+      <p className="text-3xl text-green-400">Custom not-found Page</p>
+      <Link href="/">
+        <Button text="Go Home" />
+      </Link>
     </div>
   );
 };

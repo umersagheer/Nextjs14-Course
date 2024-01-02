d18312b36a68b546994fd68aa8e6cd52f1f9d018
Muhammad Umer <mhadisb1212@gmail.com>
2024-01-03 02:19:20 +0500

    Metadata

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,8 +1,5 @@
-"use client";
-
 import Link from "next/link";
-import React from "react";
-import { useRouter } from "next/navigation";
+import { Metadata } from "next";
 
 type paramsType = {
   params: params;
@@ -11,17 +8,21 @@ type paramsType = {
 type params = {
   productId: string;
 };
-const ProductDetails = ({ params }: paramsType) => {
-  const router = useRouter();
 
+export const generateMetadata = ({ params }: paramsType): Metadata => {
+  return {
+    title: `Product ${params.productId}`,
+  };
+};
+
+const ProductDetails = ({ params }: paramsType) => {
   return (
     <>
       <h1 className="border bg-blue-500 px-3 py-1 rounded-md m-2 w-fit">
         Product Number {params.productId}
       </h1>
       <Link
-        href="products"
-        onClick={() => router.back()}
+        href="/products"
         className="border bg-blue-500 px-3 py-1 rounded-md m-2 hover:bg-blue-400/80"
       >
         Back to product list
